da3808be494d74abda18d85a0ddc893924c5e9b9
Humayun kabir <humayunkabir.cep@gmail.com>
2020-02-25 15:21:45 +0600

    Web font

--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,11 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
-importScripts(
-  "/precache-manifest.df16727ab30b5f4b983a56e139518812.js"
-);
+importScripts('/precache-manifest.b4bde254df2842701ecffd830af06cfb.js');
 
-self.addEventListener('message', (event) => {
+self.addEventListener('message', event => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
@@ -33,7 +31,6 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
-  
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL('/index.html'), {
+  blacklist: [/^\/_/, /\/[^\/?]+\.[^\/]+$/]
 });
