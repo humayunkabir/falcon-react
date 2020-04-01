f802224caad1298d0bcec64c619496bb015e274a
Humayun kabir <humayunkabir.cep@gmail.com>
2020-04-01 23:32:42 +0600

    v.2.4.0

--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,11 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
-importScripts(
-  "/precache-manifest.d917ca8414ebcec7c429be9805cd55b8.js"
-);
+importScripts('/precache-manifest.4af89ce864c4e92e723a618387de5111.js');
 
-self.addEventListener('message', (event) => {
+self.addEventListener('message', event => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
@@ -33,7 +31,6 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
-  
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL('/index.html'), {
+  blacklist: [/^\/_/, /\/[^/?]+\.[^/]+$/]
 });
---
+++ static/js/10.19b13474.chunk.js
@@ -0,0 +1,293 @@
+(this['webpackJsonpfalcon-react'] = this['webpackJsonpfalcon-react'] || []).push([
+  [10],
+  {
+    1358: function(e, t, a) {
+      e.exports = a.p + 'static/media/14.2e238fd1.jpg';
+    },
+    1359: function(e, t, a) {
+      e.exports = a.p + 'static/media/19.3c467e00.jpg';
+    },
+    1360: function(e, t, a) {
+      e.exports = a.p + 'static/media/15.fe45522f.jpg';
+    },
+    1361: function(e, t, a) {
+      e.exports = a.p + 'static/media/17.5145db24.jpg';
+    },
+    1362: function(e, t, a) {
+      e.exports = a.p + 'static/media/20.c906bddb.jpg';
+    },
+    1363: function(e, t, a) {
+      e.exports = a.p + 'static/media/16.321fd6d4.jpg';
+    },
+    1364: function(e, t, a) {
+      e.exports = a.p + 'static/media/18.80c92e6e.jpg';
+    },
+    2210: function(e, t, a) {
+      'use strict';
+      a.r(t);
+      var n = a(0),
+        c = a.n(n),
+        r = a(101),
+        l = a(12),
+        s = a(30),
+        o = a(1013),
+        m = a(19),
+        i = a(493),
+        u = a(115),
+        p = a(960),
+        E = a(487),
+        f = a(500),
+        g = a(396),
+        b = a(963),
+        d = function(e) {
+          return function(t) {
+            var a = Object(n.useState)(null),
+              r = Object(m.a)(a, 2),
+              s = r[0],
+              o = r[1];
+            return c.a.createElement(
+              i.a,
+              { fluid: !0 },
+              c.a.createElement(
+                f.a,
+                { className: 'min-vh-100 bg-100' },
+                c.a.createElement(g.a, { xs: 6, className: 'd-none d-lg-block' }, s && c.a.createElement(u.a, s)),
+                c.a.createElement(
+                  g.a,
+                  { sm: 10, md: 6, className: 'px-sm-0 align-self-center mx-auto py-5' },
+                  c.a.createElement(
+                    f.a,
+                    { noGutters: !0, className: 'justify-content-center' },
+                    c.a.createElement(
+                      g.a,
+                      { lg: 9, xl: 8, className: 'col-xxl-6' },
+                      c.a.createElement(
+                        b.a,
+                        null,
+                        c.a.createElement(
+                          E.a,
+                          { className: 'bg-split-card-header bg-circle-shape text-center p-2' },
+                          c.a.createElement(
+                            l.b,
+                            {
+                              className:
+                                'text-white text-sans-serif font-weight-extra-bold fs-4 z-index-1 position-relative',
+                              to: '/'
+                            },
+                            'falcon'
+                          )
+                        ),
+                        c.a.createElement(
+                          p.a,
+                          { className: 'p-4' },
+                          c.a.createElement(e, Object.assign({ setBgProps: o }, t))
+                        )
+                      )
+                    )
+                  )
+                )
+              )
+            );
+          };
+        },
+        x = a(1358),
+        h = a.n(x),
+        N = d(function(e) {
+          var t = e.setBgProps;
+          return (
+            Object(n.useEffect)(
+              function() {
+                return t({ image: h.a, position: '50% 20%' });
+              },
+              [t]
+            ),
+            c.a.createElement(
+              n.Fragment,
+              null,
+              c.a.createElement(
+                s.a,
+                { align: 'center', justify: 'between' },
+                c.a.createElement('h3', null, 'Login'),
+                c.a.createElement(
+                  'p',
+                  { className: 'mb-0 fs--1' },
+                  c.a.createElement('span', { className: 'font-weight-semi-bold' }, 'New User? '),
+                  c.a.createElement(l.b, { to: '/authentication/split/register' }, 'Create account')
+                )
+              ),
+              c.a.createElement(o.a, { layout: 'split', hasLabel: !0 })
+            )
+          );
+        }),
+        j = a(1048),
+        y = a(1359),
+        v = a.n(y),
+        w = d(function(e) {
+          var t = e.setBgProps;
+          return (
+            Object(n.useEffect)(
+              function() {
+                return t({ image: v.a });
+              },
+              [t]
+            ),
+            c.a.createElement(
+              'div',
+              { className: 'text-center' },
+              c.a.createElement(j.a, { layout: 'split', titleTag: 'h3' })
+            )
+          );
+        }),
+        O = a(1017),
+        B = a(1360),
+        P = a.n(B),
+        k = d(function(e) {
+          var t = e.setBgProps;
+          return (
+            Object(n.useEffect)(
+              function() {
+                return t({ image: P.a });
+              },
+              [t]
+            ),
+            c.a.createElement(
+              n.Fragment,
+              null,
+              c.a.createElement(
+                s.a,
+                { align: 'center', justify: 'between' },
+                c.a.createElement('h3', null, 'Register'),
+                c.a.createElement(
+                  'p',
+                  { className: 'mb-0 fs--1' },
+                  c.a.createElement('span', { className: 'font-weight-semi-bold' }, 'Already User? '),
+                  c.a.createElement(l.b, { to: '/authentication/split/login' }, 'Login')
+                )
+              ),
+              c.a.createElement(O.a, { layout: 'split', hasLabel: !0 })
+            )
+          );
+        }),
+        L = a(1050),
+        z = a(1361),
+        F = a.n(z),
+        J = d(function(e) {
+          var t = e.setBgProps;
+          return (
+            Object(n.useEffect)(
+              function() {
+                return t({ image: F.a, position: '50% 76%', overlay: !0 });
+              },
+              [t]
+            ),
+            c.a.createElement(
+              'div',
+              { className: 'text-center' },
+              c.a.createElement('h4', { className: 'mb-0' }, ' Forgot your password?'),
+              c.a.createElement('small', null, "Enter your email and we'll send you a reset link."),
+              c.a.createElement(L.a, { layout: 'split' })
+            )
+          );
+        }),
+        R = a(1051),
+        T = a(1362),
+        U = a.n(T),
+        A = d(function(e) {
+          var t = e.setBgProps;
+          return (
+            Object(n.useEffect)(
+              function() {
+                return t({ image: U.a });
+              },
+              [t]
+            ),
+            c.a.createElement(
+              'div',
+              { className: 'text-center' },
+              c.a.createElement('h3', null, 'Reset password'),
+              c.a.createElement(R.a, { layout: 'split', hasLabel: !0 })
+            )
+          );
+        }),
+        C = a(1052),
+        G = a(1363),
+        H = a.n(G),
+        S = d(function(e) {
+          var t = e.setBgProps;
+          return (
+            Object(n.useEffect)(
+              function() {
+                return t({ image: H.a, position: '50% 30%' });
+              },
+              [t]
+            ),
+            c.a.createElement(
+              'div',
+              { className: 'text-center' },
+              c.a.createElement(C.a, { email: 'xyz@abc.com', layout: 'split', titleTag: 'h3' })
+            )
+          );
+        }),
+        q = a(110),
+        D = a(1053),
+        I = a(263),
+        K = a(129),
+        M = a.n(K),
+        Q = a(1364),
+        V = a.n(Q),
+        W = d(function(e) {
+          var t = e.setBgProps;
+          return (
+            Object(n.useEffect)(
+              function() {
+                return t({ image: V.a });
+              },
+              [t]
+            ),
+            c.a.createElement(
+              f.a,
+              { className: 'justify-content-center' },
+              c.a.createElement(
+                g.a,
+                { xs: 'auto' },
+                c.a.createElement(
+                  I.a,
+                  { className: 'align-items-center' },
+                  c.a.createElement(q.a, { src: M.a, size: '4xl', className: 'mr-4' }),
+                  c.a.createElement(
+                    I.a,
+                    { body: !0 },
+                    c.a.createElement('h4', null, 'Hi! Emma'),
+                    c.a.createElement(
+                      'p',
+                      { className: 'mb-0' },
+                      'Enter your password ',
+                      c.a.createElement('br', null),
+                      'to access the admin.'
+                    )
+                  )
+                ),
+                c.a.createElement(D.a, { className: 'mt-4' })
+              )
+            )
+          );
+        });
+      t.default = function(e) {
+        var t = e.match.url;
+        return c.a.createElement(
+          r.d,
+          null,
+          c.a.createElement(r.b, { path: ''.concat(t, '/login'), exact: !0, component: N }),
+          c.a.createElement(r.b, { path: ''.concat(t, '/logout'), exact: !0, component: w }),
+          c.a.createElement(r.b, { path: ''.concat(t, '/register'), exact: !0, component: k }),
+          c.a.createElement(r.b, { path: ''.concat(t, '/forget-password'), exact: !0, component: J }),
+          c.a.createElement(r.b, { path: ''.concat(t, '/confirm-mail'), exact: !0, component: S }),
+          c.a.createElement(r.b, { path: ''.concat(t, '/password-reset'), exact: !0, component: A }),
+          c.a.createElement(r.b, { path: ''.concat(t, '/lock-screen'), exact: !0, component: W }),
+          c.a.createElement(r.a, { to: '/errors/404' })
+        );
+      };
+    }
+  }
+]);
+//# sourceMappingURL=10.19b13474.chunk.js.map
